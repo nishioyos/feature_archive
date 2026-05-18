e41cfbe7107546ddee49fcb25ff8b71bf9444ab2
nishioyos <nishio_yos@palgroup.co.jp>
2026-05-18 16:26:02 +0900

    Create build.js

---
+++ build.js
@@ -0,0 +1,19 @@
+const https = require("https");
+const fs = require("fs");
+
+const SERVICE_ID = process.env.MICROCMS_SERVICE_ID;
+const API_KEY = process.env.MICROCMS_API_KEY;
+
+const url = `https://${SERVICE_ID}.microcms.io/api/v1/news?limit=100`;
+
+https.get(url, { headers: { "X-MICROCMS-API-KEY": API_KEY } }, (res) => {
+  let data = "";
+  res.on("data", (chunk) => data += chunk);
+  res.on("end", () => {
+    const json = JSON.parse(data);
+
+    fs.mkdirSync("dist", { recursive: true });
+    fs.writeFileSync("dist/news.json", JSON.stringify(json.contents, null, 2));
+    console.log("JSON generated!");
+  });
+});
