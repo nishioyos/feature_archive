6a44af7791a58eab22117637aebc1cffab223691
nishioyos <nishio_yos@palgroup.co.jp>
2026-05-19 10:07:46 +0900

    Update build.js

--- build.js
+++ build.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 const SERVICE_ID = process.env.MICROCMS_SERVICE_ID;
 const API_KEY = process.env.MICROCMS_API_KEY;
 
-const url = `https://${SERVICE_ID}.microcms.io/api/v1/news?limit=100`;
+const url = `https://${SERVICE_ID}.microcms.io/api/v1/archive?limit=100`;
 
 https.get(url, { headers: { "X-MICROCMS-API-KEY": API_KEY } }, (res) => {
   let data = "";
   res.on("data", (chunk) => data += chunk);
   res.on("end", () => {
     const json = JSON.parse(data);
-    const contents = json.contents || []; // ← 空配列をデフォルトに
+    const contents = json.contents || [];
 
     fs.mkdirSync("dist", { recursive: true });
     fs.writeFileSync("dist/news.json", JSON.stringify(contents, null, 2));
